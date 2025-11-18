9afec572fc266374a27b8ec51d412b09914471ae
TomasOliver <tomascampanelli@hotmail.com>
2025-11-18 13:01:56 -0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,30 @@
 import { Routes } from '@angular/router';
 import { TeamsSelectionComponent } from './equipos/teams-selection-component/teams-selection-component';
-import { PlantelComponent } from './equipos/plantel-component/plantel-component'; 
+import { PlantelComponent } from './equipos/plantel-component/plantel-component';
 import { LoginComponent } from './auth/login/login.component';
 import { AuthGuard } from './guards/auth-guard';
 import { RegisterComponent } from './auth/register/register.component';
+import { MenuComponent } from './menu-principal/menu-principal.component';
 
 
 export const routes: Routes = [
-    {path: 'listaTeams', component: TeamsSelectionComponent},
-    {path: 'listaTeams/:id', component: PlantelComponent},
-    
   {
-    path: 'login',
-    loadComponent: () =>
-      import('./auth/login/login.component').then(m => m.LoginComponent)
+    path: 'listaTeams',
+    canActivate: [AuthGuard],
+    component: TeamsSelectionComponent
   },
-   { path: 'register', component: RegisterComponent },
+  {
+    path: 'listaTeams/:id',
+    canActivate: [AuthGuard],
+    component: PlantelComponent
+  },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'menu',
     canActivate: [AuthGuard],
-    loadComponent: () =>
-      import('./menu-principal/menu-principal.component').then(m => m.MenuComponent)
+    component: MenuComponent
   },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'login' }
